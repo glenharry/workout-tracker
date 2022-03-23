426d96afbb6b2be243806ad2132ccb8845f8cb3a
glen-harry <glenharry84@gmail.com>
2022-03-23 23:25:46 +1100

    added for loop for to print exercises

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 // Exercise Form inputs
 const exerciseNameInput = document.querySelector("#name");
+const weight = document.querySelector("#weight");
 const setOneInput = document.querySelector("#setOneReps");
 const setTwoInput = document.querySelector("#setTwoReps");
 const setThreeInput = document.querySelector("#setThreeReps");
+const setWeightOne = document.querySelector("#setWeightOne");
+const setWeightTwo = document.querySelector("#setWeightTwo");
+const setWeightThree = document.querySelector("#setWeightThree");
 
 // Exercise List
 const exerciseList = document.querySelector(".exercise-list");
@@ -17,6 +21,7 @@ let exercises = [];
 function handleButtonClick(e) {
     e.preventDefault();
     createExercise();
+    // exercises.forEach(printExercises);
     printExercises();
 }
 
@@ -26,21 +31,39 @@ function createExercise() {
 
     // Create Exercise Class
     class exercise {
-        constructor(id, name, setOneReps, setTwoReps, setThreeReps) {
+        constructor(
+            id,
+            name,
+            weight,
+            setOneReps,
+            setTwoReps,
+            setThreeReps,
+            setWeightOne,
+            setWeightTwo,
+            setWeightThree
+        ) {
             this.id = id;
             this.name = name;
+            this.weight = weight;
             this.setOneReps = setOneReps;
             this.setTwoReps = setTwoReps;
             this.setThreeReps = setThreeReps;
+            this.setWeightOne = setWeightOne;
+            this.setWeightTwo = setWeightTwo;
+            this.setWeightThree = setWeightThree;
         }
     }
     // Assigns values to newly created class from form inputs
     let ex1 = new exercise(
         exerciseID,
         exerciseNameInput.value,
+        weight.value,
         setOneInput.value,
         setTwoInput.value,
-        setThreeInput.value
+        setThreeInput.value,
+        setWeightOne.value,
+        setWeightTwo.value,
+        setWeightThree.value
     );
     exercises.push(ex1);
     // Test
@@ -48,19 +71,19 @@ function createExercise() {
 }
 
 function printExercises() {
-    let exerciseListItem = ``;
-    exerciseListItem = `
-<li>
-<h3 class="exercise-list--name"></h3>
-<h4 class="exercise-list--weight"></h4>
-<ul class="exercise-list--set">
-    <li class="exercise-list--reps"></li>
-    <li class="exercise-list--reps"></li>
-    <li class="exercise-list--reps"></li>
-</ul>
-</li>
-`;
-    document.createElement("li");
+    let li = document.createElement("li");
+    for (let i = 0; i < exercises.length; i += 1) {
+        li.innerHTML = `
+            <h3 class="exercise-list--name">${exercises[i].name}</h3>
+            <h4 class="exercise-list--weight">${exercises[i].weight}</h4>
+            <ul class="exercise-list--set">
+                <li class="exercise-list--reps">${exercises[i].setOneReps} @ ${exercises[i].weight}</li>
+                <li class="exercise-list--reps">${exercises[i].setTwoReps} @ ${exercises[i].weight}</li>
+                <li class="exercise-list--reps">${exercises[i].setThreeReps} @ ${exercises[i].weight}</li>
+            </ul>
+        `;
+        exerciseList.appendChild(li);
+    }
 }
 
 // Exercise submit button
