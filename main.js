a7dc8c1d491b86056adb2d8277efeadb845d1145
glen-harry <glenharry84@gmail.com>
2022-03-27 09:38:33 +1100

    Pre Array conversion

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Exercise Form inputs
 const exerciseNameInput = document.querySelector("#name");
 const weight = document.querySelector("#weight");
+const plannedReps = document.querySelector("#plannedReps");
 const setOneInput = document.querySelector("#setOneReps");
 const setTwoInput = document.querySelector("#setTwoReps");
 const setThreeInput = document.querySelector("#setThreeReps");
@@ -40,7 +41,9 @@ function createExercise() {
             setThreeReps,
             setWeightOne,
             setWeightTwo,
-            setWeightThree
+            setWeightThree,
+            plannedVolume,
+            plannedReps
         ) {
             this.id = id;
             this.name = name;
@@ -51,9 +54,12 @@ function createExercise() {
             this.setWeightOne = setWeightOne;
             this.setWeightTwo = setWeightTwo;
             this.setWeightThree = setWeightThree;
+            this.plannedVolume = plannedVolume;
+            this.plannedReps = plannedReps;
         }
     }
     // Assigns values to newly created class from form inputs
+
     let ex1 = new exercise(
         exerciseID,
         exerciseNameInput.value,
@@ -63,31 +69,83 @@ function createExercise() {
         setThreeInput.value,
         setWeightOne.value,
         setWeightTwo.value,
-        setWeightThree.value
+        setWeightThree.value,
+        (plannedVolume =
+            plannedReps.value *
+            (parseInt(setWeightOne.value) +
+                parseInt(setWeightTwo.value) +
+                parseInt(setWeightThree.value))),
+        plannedReps.value
     );
     exercises.push(ex1);
     // Test
     console.log(exercises);
 }
-
 function printExercises() {
     let li = document.createElement("li");
     for (let i = 0; i < exercises.length; i += 1) {
         li.innerHTML = `
             <h3 class="exercise-list--name">${exercises[i].name}</h3>
-            <h4 class="exercise-list--weight">${exercises[i].weight}</h4>
+            <h4 class="exercise-list--weight">Planned Weight ${exercises[i].weight}</h4>
+            <h5 class="exercise-list--planned-volume">Planned Volume: ${plannedVolume}</h5>
+            <h5 class="exercise-list--total-volume">Total Volume: ${exercises[i].weight}</h5>
             <ul class="exercise-list--set">
-                <li class="exercise-list--reps">${exercises[i].setOneReps} @ ${exercises[i].weight}</li>
-                <li class="exercise-list--reps">${exercises[i].setTwoReps} @ ${exercises[i].weight}</li>
-                <li class="exercise-list--reps">${exercises[i].setThreeReps} @ ${exercises[i].weight}</li>
+                <li class="exercise-list--reps">${exercises[i].setOneReps} @ ${exercises[i].setWeightOne}</li>
+                <li class="exercise-list--reps">${exercises[i].setTwoReps} @ ${exercises[i].setWeightTwo}</li>
+                <li class="exercise-list--reps">${exercises[i].setThreeReps} @ ${exercises[i].setWeightThree}</li>
             </ul>
         `;
         exerciseList.appendChild(li);
     }
 }
 
+// Prefil Rep weight Function
+function prefillRepWeight(setWeight) {
+    setWeight = weight.value;
+    setWeightOne.value = setWeight;
+    setWeightTwo.value = setWeight;
+    setWeightThree.value = setWeight;
+    console.log(setWeight);
+}
+
 // Exercise submit button
 button.addEventListener("click", handleButtonClick);
 
-// Functionality ideas
-//   KG moved per minute stats
+// Exercise weight field
+weight.addEventListener("change", prefillRepWeight);
+
+// Tests
+function test() {
+    function prefillFields() {
+        (exerciseNameInput.value = "Press Up"),
+            (weight.value = 20),
+            (plannedReps.value = 10),
+            (setOneInput.value = 10),
+            (setTwoInput.value = 10),
+            (setThreeInput.value = 10),
+            (setWeightOne.value = weight.value),
+            (setWeightTwo.value = weight.value),
+            (setWeightThree.value = 30);
+    }
+    prefillFields();
+    createExercise();
+    printExercises();
+}
+test();
+
+// Functionality ToDos
+// Functionality ToDos
+// Functionality ToDos
+
+// IDEA
+//  Add in a PLUS button to add additional sets
+//      Convert setOneInput, setTwoInput, setThreeInput and setWeightOne, setWeightTwo, setWeightThree
+//      into arrays. Then when you click the PLUS button it will just push a new item to the array
+
+// IDEA
+// Do I want to hard record all of the data into some form of a table?
+// Or
+// Do I want to create set Routines that I can fill out?
+
+// IDEA
+// Once you have hit your expected volume a few times in a row. The app should reccomend a new volume.
